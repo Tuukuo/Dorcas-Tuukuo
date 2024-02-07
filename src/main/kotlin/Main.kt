7ae246a7dede62fb3ac2c443460e71663bed1c32
Tuukuo <tuukuodorcas@gmail.com>
2024-02-07 23:04:53 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+import java.security.KeyStore.TrustedCertificateEntry
+
+fun main() {
+   var fullName = "Dorcas Tuukuo"
+   println("studentName")
+   val studentAge = 20
+   println(studentAge)
+   val studentNumber = +254720345768
+   println("studentNumber")
+   var weightKg  = 47.3
+   println("weightInKg")
+   var kenyanCitizen = true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
