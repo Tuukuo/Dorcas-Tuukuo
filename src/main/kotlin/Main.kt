6762058f7d41c1841c1290801d52f127b0da1220
Tuukuo <tuukuodorcas@gmail.com>
2024-02-10 17:20:38 +0300

    added file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,16 @@
 import java.security.KeyStore.TrustedCertificateEntry
 
-fun main() {
+fun main(){
    var fullName = "Dorcas Tuukuo"
-   println("studentName")
-   val studentAge = 20
-   println(studentAge)
-   val studentNumber = +254720345768
-   println("studentNumber")
-   var weightKg  = 47.3
-   println("weightInKg")
-   var kenyanCitizen = true
+   println(fullName)
+   var age : Byte = 20
+   println(age)
+   var phoneNumber : String = "0723231465"
+   println(phoneNumber)
+   var weightKg = 47.3F
+   println(weightKg)
+   var kenyanCitizen : Boolean = true
+   println(kenyanCitizen)
 
 
 
